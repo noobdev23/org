257d58436b659f1682b6be68d28b3ab3b87d393c
ronitn <123225727+ronitn@users.noreply.github.com>
2023-03-21 10:01:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,3 +31,68 @@ function calculateLoanAmount()
     else
         alert("Please Check Entered Amount");
 }
+
+function calculateAmortization(){
+    var loanAmount=parseFloat(document.getElementById("amount").value);
+    var interestRate=parseFloat(document.getElementById("interest").value);
+    var years=parseFloat(document.getElementById("years").value);
+
+    var monthlyInterestRate=interestRate/(100*12);
+    var numberOfPayments=years*12;
+
+    var monthlyPayment=calculateLoanAmount();
+
+    var tableBody=document.getElementById("amortization-body");
+
+    var balance=loanAmount;
+    var today=new Date();
+    var totalInterest=0;
+
+    tableBody.innerHTML="";
+
+    for(var i=0; i<numberOfPayments; i++){
+        var interest=balance*monthlyInterestRate;
+        var principal=monthlyPayment-interest;
+        balance-=principal;
+        totalInterest+=interest;
+
+        var date=new Date(today.getTime());
+        date.setMonth(today.getMonth()+i+1);
+
+        var tableRow=document.createElement("tr");
+
+        var paymentNumberColumn=document.createElement("td");
+        paymentNumberColumn.innerHTML=i+1;
+
+        var dateColumn=document.createElement("td");
+        dateColumn.innerHTML=(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
+
+        var startingBalanceColumn=document.createElement("td");
+        startingBalanceColumn.innerHTML=balance+principal;
+
+        var paymentColumn=document.createElement("td");
+        paymentColumn.innerHTML=monthlyPayment.toFixed(2);
+
+        var principalColumn=document.createElement("td");
+        principalColumn.innerHTML=principal.toFixed(2);
+
+        var interestColumn=document.createElement("td");
+        interestColumn.innerHTML=interest.toFixed(2);
+
+        var endingBalanceColumn=document.createElement("td");
+        endingBalanceColumn.innerHTML=balance.toFixed(2);
+
+        tableRow.appendChild(paymentNumberColumn);
+        tableRow.appendChild(dateColumn);
+        tableRow.appendChild(startingBalanceColumn);
+        tableRow.appendChild(paymentColumn);
+        tableRow.appendChild(principalColumn);
+        tableRow.appendChild(interestColumn);
+        tableRow.appendChild(endingBalanceColumn);
+
+        tableBody.appendChild(tableRow);
+    }
+
+    document.getElementById("total-interest").value=totalInterest.toFixed(2);
+    document.getElementById("amortization").style.display="block";
+}
